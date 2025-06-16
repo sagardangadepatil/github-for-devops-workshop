3bc7e43b8230a03c8186aa058a0f0e27bb052fae
sagardangadepatil <dangadesagar@gmail.com>
2025-06-16 07:03:10 +0530

    print the actual datatype

--- python/datatype.py
+++ python/datatype.py
@@ -1,4 +1,8 @@
 a = 1
 b = 2
-print(type(a))
-# type will showcase type of datatype
+c = 10.5
+d = "Jhon"
+print(type(a)) # type will print type of datatype
+print(type(b)) # type will print type of datatype
+print(type(c)) # type will print type of datatype
+print(type(d)) # type will print type of datatype
