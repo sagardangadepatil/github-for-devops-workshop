87c024c4f493e0ea4c24203bf85d1406d28e9d7d
sagardangadepatil <dangadesagar@gmail.com>
2025-06-16 19:40:12 +0530

    concating two tuples

---
+++ python/tuple.py
@@ -0,0 +1,6 @@
+mytuple = ('a', 'b', 'c', 'd')
+print(mytuple)
+mytuple1 = ('e', 'f', 'g')
+print(mytuple1)
+mytuple+=mytuple1 #Concatination of two tuples variables
+print("concatination of two tuples as:", mytuple) 
