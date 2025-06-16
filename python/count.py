bbdc4e131b1fff5b51b79691c4acc26202c45d03
sagardangadepatil <dangadesagar@gmail.com>
2025-06-16 19:23:00 +0530

    length i.e. len and count function

---
+++ python/count.py
@@ -0,0 +1,8 @@
+name = "Hello Good Morning All"
+name1 = "Hope you are excited for Python"
+
+#How to Count character present in strings or variables?
+print("Length of name is:" ,len(name))  #len function will be used to calculate length of string including space
+print("Length of name1 is:" ,len(name1))  #len function will be used to calculate length of string including space
+
+print(name.count("o"))  #Calculate "o" character from name string
--- python/string_concate.py
+++ python/string_concate.py
@@ -3,3 +3,5 @@ str2 = "World"
 result = str1 + " " + str2        #Concate the strings
 print(result)
 print(result.upper())             # Changing case from lower to upper
+length = len(result)            #defining length variable.
+print("length of string:", length) # capture and display length of the string
