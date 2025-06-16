debc84d58b87b60f4c08c88084b3475c8ec959c7
sagardangadepatil <dangadesagar@gmail.com>
2025-06-16 21:01:46 +0530

    Dictionary functions of Data types

---
+++ python/dictionary.py
@@ -0,0 +1,11 @@
+#A group of values within {}
+myDict = { 1: 'apple', 2: 'mango'}
+print(myDict)
+#Create empty dictionary
+myDist1 = {}
+print(myDist1)
+#Integer keys - Dictionary with integer keys
+print("Dictionary with integer keys ", myDict)
+#Mixed keys - Dictionary with mixed keys
+myDist2 = {'name': "Jhon", "Hight in Cm": 160, "Weight in Pund": 105 }
+print("Dictionary with mixed keys ", myDist2)
