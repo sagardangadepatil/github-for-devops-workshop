4f14943a3c4aa200ff454f5a5ef71bccecf38c62
sagardangadepatil <dangadesagar@gmail.com>
2025-06-16 19:59:59 +0530

    tuple and index function

--- python/tuple.py
+++ python/tuple.py
@@ -4,3 +4,6 @@ mytuple1 = ('e', 'f', 'g')
 print(mytuple1)
 mytuple+=mytuple1 #Concatination of two tuples variables
 print("concatination of two tuples as:", mytuple) 
+#Indexing character - from mytuple string
+print("Display value from tuple using Index value captured as", mytuple[5])
+print("Display first 5 value or slice from tuple using Index value captured as", mytuple[0:5]) #We also called this as slicing from tuple
