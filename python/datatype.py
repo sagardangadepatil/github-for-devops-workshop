38f84c7776af130576b6c4f1229a9e99b105ec93
sagardangadepatil <dangadesagar@gmail.com>
2025-06-16 19:21:26 +0530

    modifying datatype.py

--- python/datatype.py
+++ python/datatype.py
@@ -6,3 +6,4 @@ print(type(a)) # type will print type of datatype
 print(type(b)) # type will print type of datatype
 print(type(c)) # type will print type of datatype
 print(type(d)) # type will print type of datatype
+#Where type function will shows actual type of data i.e. int, float, string etc
