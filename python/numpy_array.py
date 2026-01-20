a31923ef675ca7d37c67635ef9ab52ce42db2e77
sagardangadepatil <dangadesagar@gmail.com>
2026-01-20 14:53:20 +0530

    Adding Inspection on numpy array

--- python/numpy_array.py
+++ python/numpy_array.py
@@ -3,7 +3,14 @@ import numpy as np  #defining numpy as np as shortcut so we can use in future co
 print("This is 1-dimensional array:")
 a = np.array([1,2,3])    #this is 1-dimensional array 
 print(a)
-print("his is and 2-dimensional array:")
+print("This is and 2-dimensional array:")
 b = np.array([[1,2,3], [4,5,6]])     #This is and 2-dimensional array.
 print(b)
 
+#numpy array inspection:
+#Checking Size of Array
+c = np.array([[1,2,3], [4,5,6]])
+print("Inspecting the Array : Checking size of Array")
+print(c)
+print("Size of Array:")
+print(c.shape)  # Gives size of Array
