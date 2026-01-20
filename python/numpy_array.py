7334881f5f11ba5bd35f010eb4e7025343abaf7f
sagardangadepatil <dangadesagar@gmail.com>
2026-01-20 12:36:28 +0530

    modifying numpy_array file

--- python/numpy_array.py
+++ python/numpy_array.py
@@ -1,8 +1,9 @@
 #Numpy is an algebra operations in python
 import numpy as np  #defining numpy as np as shortcut so we can use in future code
-a = numpy.array([1,2,3])    #this is 1-dimensional array 
+print("This is 1-dimensional array:")
+a = np.array([1,2,3])    #this is 1-dimensional array 
 print(a)
-
-b = numpy.array([[1,2,3], [4,5,6]])     #This is and 2-dimensional array.
+print("his is and 2-dimensional array:")
+b = np.array([[1,2,3], [4,5,6]])     #This is and 2-dimensional array.
 print(b)
 
