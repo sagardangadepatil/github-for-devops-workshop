3cfd3b1c98230fa43715c0d65adc5aac327a8339
sagardangadepatil <dangadesagar@gmail.com>
2026-01-22 20:50:48 +0530

    updating file

--- python/numpy_math.py
+++ python/numpy_math.py
@@ -14,7 +14,27 @@ a = int(input("Type First Number :"))
 b = int(input("Type Second Number :"))
 print("First Number you entered is:", a)
 print("Second Number you entered is:", b)
-print("Addition of First Number and Second number is : ")
-
 c = int(np.sum([a,b]))
 print(c)
+print("Addition of First Number and Second number is : ", c)
+
+c = np.sum([[1,2], [5,6]], axis= 0)
+print("Addition of Array first number with first number of OTHER array and vice versa using axis =0 : ",c) 
+
+c = np.sum([[1,2], [5,6]], axis= 1)
+print("Addition of Array first number with second number of SAME array and vice versa using axis =1 : ",c) 
+
+print("Enter 1-dimentional array :")
+
+# Accept array elements from user separated by space
+arr1 = input("Enter elements of the array separated by space: ").split()
+arr2 = input("Enter elements of the array separated by space: ").split()
+# Convert input strings to integers
+arr1 = [int(x) for x in arr1]
+arr2 = [int(y) for y in arr2]
+
+# Print the array
+print("The First array you entered is:", arr1)
+print("The Second array you entered is:", arr2)
+d = np.sum([arr1, arr2])
+print("Addition of array like first number with first number of other array :", d)
