ffadc2dc8975069e4228773fbaae05a4ee83ce76
sagardangadepatil <dangadesagar@gmail.com>
2026-02-24 19:31:35 +0530

    Adding Numpy Broadcast operations under file numpy_broadcast.py

---
+++ python/numpy_broadcast.py
@@ -0,0 +1,7 @@
+import numpy as np
+#Concept of Broadcast
+a = np.array([[0,0,0],[1,2,3],[4,5,6],[7,8,9]])
+b = np.array([0,1,2])
+print("First Array is avilable with us is : \n", a,'\n')
+print("Second Array is avilable with us is : \n", b, '\n')
+print("First Array + Second Array = \n", a+b, '\n')
